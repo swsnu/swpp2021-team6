ab34a8554baf2101c7b555dd55b2e28fba84ebc3
Juyoung <godante27@gmail.com>
2021-11-03 03:42:04 +0900

    style: Replace css with scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Main from './pages/Main';
