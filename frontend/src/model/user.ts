84ac2a06841af4a2ff2b4ad6c9fb1eb8124f3cca
Juyoung <godante27@gmail.com>
2021-11-04 15:39:46 +0900

    chore: Add files for types

---
+++ frontend/src/model/user.ts
@@ -0,0 +1,10 @@
+export interface UserEntity {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+  gu: string;
+  dong: string;
+  gender: string;
+  introduction: string;
+}
