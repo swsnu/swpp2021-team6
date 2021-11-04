f2ea3408adde95143f03e921602baeb805293efc
Juyoung <godante27@gmail.com>
2021-11-04 15:43:32 +0900

    feat: Add redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+// import axios from 'axios';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store, { history } from './store/store';
+
+// CSRF TOKEN
+// axios.defaults.xsrfCookieName = 'csrftoken';
+// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
   document.getElementById('root'),
 );
 
