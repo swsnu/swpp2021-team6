19b5db74a13dfb215abfa221a4c7b4491fab3a72
Juyoung <godante27@gmail.com>
2021-11-05 16:02:47 +0900

    chore: Add post type

--- frontend/src/model/post.ts
+++ frontend/src/model/post.ts
@@ -1,1 +1,31 @@
-export interface PostEntity {}
+import { exerciseType, expectedLevelType } from './type';
+
+export interface PostEntity {
+  exerciseType: exerciseType;
+  expectedLevel: expectedLevelType;
+  meetAt: string;
+  title: string;
+  description: string;
+  appointmentTime: string;
+  minCapacity: number;
+  maxCapacity: number;
+  kakaotalkLink: string;
+  status: string;
+  latitude: number;
+  logitude: number;
+}
+
+export interface PostInputDTO {
+  exerciseType: number;
+  expectedLevel: expectedLevelType;
+  meetAt: string;
+  title: string;
+  description: string;
+  appointmentTime: string;
+  minCapacity: number;
+  maxCapacity: number;
+  kakaotalkLink: string;
+  status: string | null;
+  latitude: number;
+  logitude: number;
+}
