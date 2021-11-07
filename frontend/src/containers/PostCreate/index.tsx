bbfb5127c39d9f027ba1426c676e53d11d0522ad
Juyoung <godante27@gmail.com>
2021-11-07 16:52:30 +0900

    fix: Change center position when dragging map

--- frontend/src/containers/PostCreate/index.tsx
+++ frontend/src/containers/PostCreate/index.tsx
@@ -4,12 +4,14 @@ import moment, { Moment } from 'moment';
 import { PostInputDTO } from '../../model/post';
 import { kakao } from '../../utils/map';
 import 'antd/dist/antd.css';
-import imageUrl from '../../assets/map/current-location-marker.png';
+// import imageUrl from '../../assets/map/current-location-marker.png';
+import profile from '../../mocks/profile.json';
+import { exerciseType } from '../../model/type';
 
 const PostCreate = () => {
   const initialState: PostInputDTO = {
-    exerciseType: 1,
-    expectedLevel: 'low',
+    exerciseType: '축구',
+    expectedLevel: '하',
     meetAt: '',
     title: '',
     description: '',
@@ -23,58 +25,71 @@ const PostCreate = () => {
   const [newPost, setNewPost] = useState<PostInputDTO>(initialState);
   const [date, setDate] = useState<Moment | null>(moment());
   const [time, setTime] = useState<Moment | null>(moment('7:00', 'h:mm a'));
-  const [curLat, setCurLat] = useState<number>(37.450035);
-  const [curLng, setCurLng] = useState<number>(126.95252);
   const [inputValue, setInputValue] = useState<string>();
   const [keyword, setKeyword] = useState<string>();
-  const [changedLat, setChangedLat] = useState<string>('37.450035');
-  const [changedLng, setChangedLng] = useState<string>('126.95252');
+  // const [changedLat, setChangedLat] = useState<number>(profile.latitude);
+  // const [changedLng, setChangedLng] = useState<number>(profile.longitude);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [center, setCenter] = useState({
+    La: profile.latitude,
+    Ma: profile.longitude,
+  });
 
-  // 현재 위치 가져오기
+  // useEffect(() => {
+  //   if ('geolocation' in navigator) {
+  //     console.log('Available');
+  //   } else {
+  //     console.log('Not Available');
+  //   }
+
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     setCurLat(position.coords.latitude);
+  //     setCurLng(position.coords.longitude);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   setCenter(position.Ma);
+  //   setChangedLng(position.La);
+  //   console.log(changedLat, changedLng);
+  // }, [position]);
+
   useEffect(() => {
-    if ('geolocation' in navigator) {
-      console.log('Available');
-    } else {
-      console.log('Not Available');
-    }
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      setCurLat(position.coords.latitude);
-      setCurLng(position.coords.longitude);
-    });
-  }, []);
+    console.log(center);
+  }, [center]);
 
   useEffect(() => {
     const container = document.getElementById('map');
-    // 현재 위치를 중심으로 하는 지도 생성
-    const currentLocation = new window.kakao.maps.LatLng(curLat, curLng);
+    // 유저 프로필에 저장된 위치를 중심으로 하는 지도 생성
+    // const currentLocation = new window.kakao.maps.LatLng(
+    //   profile.latitude,
+    //   profile.longitude,
+    // );
     const options = {
-      center: currentLocation,
+      center: new window.kakao.maps.LatLng(profile.latitude, profile.longitude),
       level: 3,
     };
     const map = new window.kakao.maps.Map(container, options);
+
+    // 지도를 드래그하면 지도 중심부 위치를 저장
     kakao.maps.event.addListener(map, 'dragend', () => {
-      const position = map.getCenter();
-      setChangedLat(position.Ma.toString());
-      setChangedLng(position.La.toString());
+      setCenter(map.getCenter());
     });
 
     // 커스텀 마커 이미지 세팅
-    const imageSrc = imageUrl;
-    const imageSize = new kakao.maps.Size(20, 20);
-    const imageOption = { offset: new kakao.maps.Point(10, 10) };
-    const markerImage = new kakao.maps.MarkerImage(
-      imageSrc,
-      imageSize,
-      imageOption,
-    );
-
-    const locationMarker = new window.kakao.maps.Marker({
-      position: currentLocation,
-      image: markerImage,
-    });
-
-    locationMarker.setMap(map);
+    // const imageSrc = imageUrl;
+    // const imageSize = new kakao.maps.Size(20, 20);
+    // const imageOption = { offset: new kakao.maps.Point(10, 10) };
+    // const markerImage = new kakao.maps.MarkerImage(
+    //   imageSrc,
+    //   imageSize,
+    //   imageOption,
+    // );
+    // const locationMarker = new window.kakao.maps.Marker({
+    //   position: currentLocation,
+    //   image: markerImage,
+    // });
+    // locationMarker.setMap(map);
 
     // 마커를 클릭하면 장소명을 표출할 인포윈도우
     const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
@@ -93,6 +108,10 @@ const PostCreate = () => {
           latitude: place.x,
           longitude: place.y,
         });
+
+        // TODO: 선택된 장소 정보를 확인하는 창과 확인 버튼 구현
+        setSelectedPlace(place);
+
         // 마커를 클릭하면 장소명이 인포윈도우에 표시
         infowindow.setContent(
           `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`,
@@ -101,14 +120,12 @@ const PostCreate = () => {
       });
     }
 
-    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    // 키워드 검색 완료 시 호출되는 콜백함수
     function placesSearchCB(data: any, status: any, pagination: any) {
       if (status === kakao.maps.services.Status.OK) {
         console.log(data);
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        const bounds = new kakao.maps.LatLngBounds();
 
+        const bounds = new kakao.maps.LatLngBounds();
         for (let i = 0; i < data.length; i++) {
           displayMarker(data[i]);
           bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
@@ -119,18 +136,16 @@ const PostCreate = () => {
       }
     }
 
-    console.log(changedLat, changedLng);
-
-    // 지도를 드래그해서 중심점이 변경된 경우 중심점을 기준으로 검색을 실행
+    // 새로운 장소 검색 개체 생성
     const ps = new kakao.maps.services.Places();
     // 키워드로 장소를 검색합니다
     ps.keywordSearch(keyword, placesSearchCB, {
-      x: changedLat,
-      y: changedLng,
+      x: center.La,
+      y: center.Ma,
       radius: 5000,
       sort: 'distance',
     });
-  }, [curLat, curLng, keyword]);
+  }, [keyword]);
 
   useEffect(() => {
     let appointmentTime;
@@ -143,7 +158,7 @@ const PostCreate = () => {
   }, [date, time]);
 
   const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setNewPost({ ...newPost, exerciseType: Number(e.target.value) });
+    setNewPost({ ...newPost, exerciseType: e.target.value });
   };
 
   const onChangeTitleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -204,13 +219,13 @@ const PostCreate = () => {
           value={newPost.exerciseType}
           onChange={(e) => onChangeSelect(e)}
         >
-          <option value={1}>축구</option>
-          <option value={2}>농구</option>
-          <option value={3}>배드민턴</option>
-          <option value={4}>테니스</option>
-          <option value={5}>탁구</option>
-          <option value={6}>러닝</option>
-          <option value={7}>자전거</option>
+          <option value="축구">축구</option>
+          <option value="농구">농구</option>
+          <option value="배드민턴">배드민턴</option>
+          <option value="테니스">테니스</option>
+          <option value="탁구">탁구</option>
+          <option value="러닝">러닝</option>
+          <option value="라이딩">라이딩</option>
         </select>
         <label htmlFor="expectedLevel">모집 실력 : </label>
         <select name="expectedLevel" id="expectedLevel" defaultValue="low">
