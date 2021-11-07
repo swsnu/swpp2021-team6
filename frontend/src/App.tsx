b0cbba69c680990efca1a33fe9545eb131d98085
Juyoung <godante27@gmail.com>
2021-11-07 15:00:05 +0900

    chore: Rename PostNew as PostCreate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,13 @@ import './App.scss';
 import SignIn from './containers/SignIn';
 import SignUp from './containers/SignUp';
 import Main from './containers/Main';
-import PostNew from './containers/PostNew';
+import PostCreate from './containers/PostCreate';
 import PostDetail from './containers/PostDetail';
 import PostEdit from './containers/PostEdit';
 import Profile from './containers/Profile';
 
 interface Props {
   history: History;
-}
-
-declare global {
-  export interface Window {
-    kakao: any;
-  }
 }
 
 const App = ({ history }: Props) => (
@@ -27,7 +21,7 @@ const App = ({ history }: Props) => (
         <Route path="/signin" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />
         <Route path="/main" exact component={Main} />
-        <Route path="/post/new" exact component={PostNew} />
+        <Route path="/post/new" exact component={PostCreate} />
         <Route path="/post/:id" exact component={PostDetail} />
         <Route path="/post/:id/edit" exact component={PostEdit} />
         <Route path="/profile" exact component={Profile} />
