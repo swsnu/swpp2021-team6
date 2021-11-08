7ca3a02ae2a20fcf13cd3478285b7bd14c88e6aa
Juyoung <godante27@gmail.com>
2021-11-08 23:17:21 +0900

    fix: Fix build error

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { getMockStore } from './test-utils/mocks';
+import { history } from './store/store';
+
+const mockStore = getMockStore({});
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={mockStore}>
+      <App history={history} />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
