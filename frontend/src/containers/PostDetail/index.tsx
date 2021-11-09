454cbe41cc3424eed94d8790024949bac431d7d7
Juyoung <godante27@gmail.com>
2021-11-09 15:43:03 +0900

    feat: Add PostDetail info box

--- frontend/src/containers/PostDetail/index.tsx
+++ frontend/src/containers/PostDetail/index.tsx
@@ -1,3 +1,42 @@
-const PostDetail = () => <h1>Post Detail Page</h1>;
+import { Avatar, Card } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import mockPost from '../../mocks/post.json';
+import changeDateFormat from '../../utils/dateToString';
+
+/* TODO
+mockPost interface 만들기
+*/
+
+const PostDetail = ({ title, description, host_id, status, ...rest }: any) => {
+  const [name, setName] = useState('');
+  const statusText = mockPost.status ? '모집 중' : '모집 완료';
+  const meetAtText = changeDateFormat(mockPost.meet_at);
+
+  return (
+    <div>
+      <h1>Post Detail Page</h1>
+      <Card>
+        <Link to={`/profile/${mockPost.host_id}`}>
+          <Avatar size="small" icon={<UserOutlined />} />
+          <span>TODO: host name with host id</span>
+        </Link>
+      </Card>
+      <Card>{statusText}</Card>
+      <h2>{mockPost.title}</h2>
+      <p>{mockPost.description}</p>
+      <Card>
+        <p>종목 : {mockPost.exercise_type}</p>
+        <p>기대 실력 : {mockPost.expected_level}</p>
+        <p>
+          모집 인원 : 최소 {mockPost.min_capacity}명 ~ 최대{' '}
+          {mockPost.max_capacity}명
+        </p>
+        <p>날짜 및 시간 : {meetAtText}</p>
+      </Card>
+    </div>
+  );
+};
 
 export default PostDetail;
