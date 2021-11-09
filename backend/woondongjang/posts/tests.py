6a6d59fe9028d46c32613cef7089c4bb6bdddab7
kyungwon-lee <55077010+kyungwon-lee@users.noreply.github.com>
2021-11-09 11:42:39 +0900

    feat: Implement posts app (#11)

---
+++ backend/woondongjang/posts/tests.py
@@ -0,0 +1,5 @@
+# from django.test import TestCast, Client
+# import json
+# from .models import Post, Participant, Exercise, Comment, Post_Exercise
+# from django.contrib.auth.models import User
+# # Create your tests here.
