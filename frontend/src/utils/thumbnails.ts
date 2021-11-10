5b211a35dfc06e019300f4a7bd869d2abcbee1cd
Juyoung <godante27@gmail.com>
2021-11-10 22:25:58 +0900

    chore: Add image assets

---
+++ frontend/src/utils/thumbnails.ts
@@ -0,0 +1,12 @@
+import soccer0 from '../assets/exercise/soccer0.jpg';
+import soccer1 from '../assets/exercise/soccer1.jpg';
+import soccer2 from '../assets/exercise/soccer2.jpg';
+import soccer3 from '../assets/exercise/soccer3.jpg';
+
+import basketball0 from '../assets/exercise/basketball0.jpg';
+import basketball1 from '../assets/exercise/basketball1.jpg';
+import basketball2 from '../assets/exercise/basketball2.jpg';
+import basketball3 from '../assets/exercise/basketball3.png';
+
+export const soccer = [soccer0, soccer1, soccer2, soccer3];
+export const basketball = [basketball0, basketball1, basketball2, basketball3];
