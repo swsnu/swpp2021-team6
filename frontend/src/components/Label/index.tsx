49fb7aba5ddd1bf14308937c05add96524d38279
Juyoung <godante27@gmail.com>
2021-11-10 22:33:03 +0900

    chore: Add className to Label component

--- frontend/src/components/Label/index.tsx
+++ frontend/src/components/Label/index.tsx
@@ -1,17 +1,21 @@
-import { ReactChildren } from 'react';
-
 interface Props {
+  className?: string;
   color: string;
   children: React.ReactNode;
 }
 
-const Label = ({ color, children }: Props) => {
+const Label = ({ className, color, children }: Props) => {
   const labelStyle = {
     padding: '5px 8px',
     backgroundColor: color,
-    borderRadius: '8px',
+    color: 'white',
   };
-  return <span style={labelStyle}>{children}</span>;
+
+  return (
+    <span className={className} style={labelStyle}>
+      {children}
+    </span>
+  );
 };
 
 export default Label;
