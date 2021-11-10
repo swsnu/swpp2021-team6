573f198b4db303fb2212b440985d6f8f5ea46a96
Juyoung <godante27@gmail.com>
2021-11-10 22:59:15 +0900

    chore: Add badminton image assets

--- frontend/src/utils/thumbnails.ts
+++ frontend/src/utils/thumbnails.ts
@@ -8,5 +8,11 @@ import basketball1 from '../assets/exercise/basketball1.jpg';
 import basketball2 from '../assets/exercise/basketball2.jpg';
 import basketball3 from '../assets/exercise/basketball3.png';
 
+import badminton0 from '../assets/exercise/badminton0.png';
+import badminton1 from '../assets/exercise/badminton1.png';
+import badminton2 from '../assets/exercise/badminton2.png';
+import badminton3 from '../assets/exercise/badminton3.png';
+
 export const soccer = [soccer0, soccer1, soccer2, soccer3];
 export const basketball = [basketball0, basketball1, basketball2, basketball3];
+export const badminton = [badminton0, badminton1, badminton2, badminton3];
