ca93bef43a61ca295e2971a1793364236752eca9
Juyoung Bae <godante27@gmail.com>
2021-11-11 03:29:37 +0900

    Add Filter container (#26)
    * chore: Add eslint rule
    
    * chore: Add Post type
    
    * chore: Add current-location-marker asset
    
    * refactor: Add onClickSetPlace function
    
    * chore: fix gitignore
    
    * feat: Add place validation before submit
    
    * chore: Fix window kakao declare
    
    * chore: Add gray color theme
    
    * chore: Remove Button component
    
    * feat: Add post filter
    
    * feat: Add Filter & Posts to MainPage

---
+++ frontend/src/assets/constants/gray.ts
@@ -0,0 +1,8 @@
+// background
+export const gray0 = '#F8F9FA';
+// border
+export const gray1 = '#E9ECEF';
+// light gray
+export const gray2 = '#ADB5BD';
+// text gray
+export const gray3 = '#868E96';
