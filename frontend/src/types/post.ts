59fc89ec554b11370a63223e8df0517b28f820b7
Juyoung <godante27@gmail.com>
2021-11-10 23:03:56 +0900

    chore: Add exercise type file

--- frontend/src/types/post.ts
+++ frontend/src/types/post.ts
@@ -1,13 +1,4 @@
-export type ExerciseType =
-  | '축구'
-  | '농구'
-  | '배드민턴'
-  | '테니스'
-  | '탁구'
-  | '러닝'
-  | '라이딩';
-
-export type ExpectedLevelType = '상' | '중' | '하' | '상관 없음';
+import { ExerciseType, ExpectedLevelType } from './exercise';
 
 export interface PostEntity {
   id: number;
