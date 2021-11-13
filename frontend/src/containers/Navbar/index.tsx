104d3cffb652fedf132ba03173f67d5f45d0c722
SunnyBaik <45480319+SunnyBaik@users.noreply.github.com>
2021-11-13 15:21:17 +0900

    Revert "Feat: Implement test for SignIn, Navbar (#47)"
    This reverts commit d760f22aa60d2757c5b7e5cc02106e3b9c03c2a1.

--- frontend/src/containers/Navbar/index.tsx
+++ frontend/src/containers/Navbar/index.tsx
@@ -23,14 +23,10 @@ const Navbar = ({ history }: NavbarProps) => {
         </span>
       </div>
       <div>
-        <span className="notification" aria-hidden="true" onClick={onClickNoti}>
+        <span aria-hidden="true" onClick={onClickNoti}>
           알림
         </span>
-        <span
-          className="mypage"
-          aria-hidden="true"
-          onClick={() => history.push('/profile')}
-        >
+        <span aria-hidden="true" onClick={() => history.push('/profile')}>
           마이페이지
         </span>
       </div>
