2d9915be7e91b40a04e199af9972cb11394a34fb
Juyoung Bae <godante27@gmail.com>
2021-11-13 17:31:47 +0900

    chore: Add test file for PostCreate container (#54)

---
+++ frontend/src/containers/PostCreate/index.test.tsx
@@ -0,0 +1,34 @@
+import * as reactRedux from 'react-redux';
+import { mount, shallow } from 'enzyme';
+import * as postActionCreators from '../../store/actions/post';
+import PostCreate from '.';
+
+const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
+
+describe('PostCreate', () => {
+  let postCreate: any;
+  let spyCreatePost: any;
+
+  beforeEach(() => {
+    postCreate = <PostCreate />;
+    useDispatchMock.mockReturnValue(jest.fn());
+    spyCreatePost = jest
+      .spyOn(postActionCreators, 'createPost')
+      .mockImplementation();
+    window.matchMedia =
+      window.matchMedia ||
+      // eslint-disable-next-line func-names
+      function () {
+        return {
+          matches: false,
+          addListener() {},
+          removeListener() {},
+        };
+      };
+  });
+
+  it('should render without error', () => {
+    const component = shallow(postCreate);
+    // expect(component.find('form').length).toBe(1);
+  });
+});
