925edf881d96425eadab54301549d31b27d60c8c
Juyoung <godante27@gmail.com>
2021-11-13 15:08:28 +0900

    feat: Implement App test for non covered line

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,22 +1,29 @@
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
+import { createMemoryHistory } from 'history';
 import App from './App';
 import { getMockStore } from './test-utils/mocks';
 import { history } from './store/store';
 
 const mockStore = getMockStore({});
 describe('App', () => {
-  let app: any;
-
-  beforeEach(() => {
-    app = (
+  it('renders without errors', () => {
+    const app = (
       <Provider store={mockStore}>
         <App history={history} />
       </Provider>
     );
+    const component = mount(app);
+    expect(component.find('.intro-container').length).toBe(1);
   });
 
-  it('renders without errors', () => {
+  it('should redirect to signin page with wrong path', () => {
+    const mockHistory = createMemoryHistory({ initialEntries: ['/hello'] });
+    const app = (
+      <Provider store={mockStore}>
+        <App history={mockHistory} />
+      </Provider>
+    );
     const component = mount(app);
     expect(component.find('.intro-container').length).toBe(1);
   });
