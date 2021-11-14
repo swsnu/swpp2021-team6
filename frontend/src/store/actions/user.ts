f708e11762347cf20835b97a40885c39cc5d0812
Juyoung <godante27@gmail.com>
2021-11-14 14:43:34 +0900

    fix: Add omitted import statement

--- frontend/src/store/actions/user.ts
+++ frontend/src/store/actions/user.ts
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 import axios from 'axios';
 
+import { push } from 'connected-react-router';
 import * as actionTypes from './actionTypes';
 import { UserEntity, UserSignInInputDTO } from '../../types/user';
 
