fcc1ce9f44d557ee7ea8f6d305149a2405ea7b95
silvermanseoul <silverman.unicorn@gmail.com>
2021-11-23 01:50:05 +0900

    feat: Remove null=True option from Comment.author

--- backend/woondongjang/posts/models.py
+++ backend/woondongjang/posts/models.py
@@ -124,7 +124,7 @@ class User_Exercise(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
