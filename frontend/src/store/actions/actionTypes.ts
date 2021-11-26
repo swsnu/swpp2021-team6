18280f350f08169080e2f88edf1866da363937e0
SunnyBaik <qortmdfuf007@hotmail.com>
2021-11-26 13:30:14 +0900

    feat/refactor: create notification model, refactor profile container

--- frontend/src/store/actions/actionTypes.ts
+++ frontend/src/store/actions/actionTypes.ts
@@ -12,4 +12,5 @@ export const SIGNIN = 'SIGNIN';
 export const SIGNOUT = 'SIGNOUT';
 export const GET_USER_INFO = 'GET_USER_INFO';
 export const GET_USER_NOTIFICATION = 'GET_USER_NOTIFICATION';
+export const READ_NOTIFICATION = 'READ_NOTIFICATION';
 // export const GET_USRS = 'GET_USERS';
