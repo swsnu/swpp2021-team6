a66266aa59560e086827dc8f873c14a13c8613e7
Juyoung <godante27@gmail.com>
2021-11-29 04:47:02 +0900

    chore: Add Post_Keyword to django admin

--- backend/woondongjang/posts/admin.py
+++ backend/woondongjang/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, User_Exercise
+from .models import Post, User_Exercise, Post_Keyword
 
 admin.site.register(Post)
 admin.site.register(User_Exercise)
+admin.site.register(Post_Keyword)
