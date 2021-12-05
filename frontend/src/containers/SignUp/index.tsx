b39e45cdac44dcbb895748617449742aa17d99d1
Juyoung <godante27@gmail.com>
2021-12-05 21:54:08 +0900

    feat: Add SignUp container

--- frontend/src/containers/SignUp/index.tsx
+++ frontend/src/containers/SignUp/index.tsx
@@ -1,192 +1,68 @@
-import { History } from 'history';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useEffect, useState } from 'react';
-import { Redirect } from 'react-router';
-import logo from '../../assets/logo.png';
-import { SignUpInputDTO } from '../../backend/entity/user';
+import { History } from 'history';
+import Layout from '../../components/Layout';
+import defaultImage from '../../assets/image/auth/signup-left.jpg';
+import Divider from '../../components/Divider';
+import Button from '../../components/Button';
 import './index.scss';
-import getGuDong from '../../utils/getGuDong';
-
-const initialFormState: SignUpInputDTO = {
-  username: '',
-  nickname: '',
-  password: '',
-  latitude: 0,
-  longitude: 0,
-  gu: '',
-  dong: '',
-  gender: '미선택',
-  introduction: '',
-  preferredExercise: [],
-};
+import googleIcon from '../../assets/image/auth/google.svg';
+import kakaotalkIcon from '../../assets/image/auth/kakaotalk.svg';
 
 interface Props {
   history: History;
 }
 
 const SignUp = ({ history }: Props) => {
-  const [form, setForm] = useState(initialFormState);
-  const [preferredExercise, setPreferredExercise] = useState({
-    exerciseName: '',
-    skillLevel: '',
-  });
+  // const [] = useState();
   const user = window.localStorage.getItem('profileInfo') || null;
+
   useEffect(() => {
     if (user) history.push('/main');
+  });
 
-    if (!('geolocation' in navigator)) {
-      alert('위치 정보를 사용할 수 없습니다. 다른 브라우저를 이용해주세요.');
-    } else {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setForm({
-          ...form,
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (form.latitude && form.longitude) {
-      getGuDong(form.longitude, form.latitude).then((value) => {
-        setForm({ ...form, gu: value.gu, dong: value.dong });
-      });
-    }
-  }, [form.latitude, form.longitude]);
-
-  useEffect(() => {
-    if (preferredExercise.exerciseName && preferredExercise.skillLevel) {
-      setForm({ ...form, preferredExercise: [preferredExercise] });
-    }
-  }, [preferredExercise]);
-
-  const onClickSubmit = () => {
-    if (!form.latitude || !form.longitude || !form.gu || !form.dong) {
-      alert('위치 정보를 불러오는 중입니다');
-    } else if (!form.username) {
-      alert('아이디를 입력해주세요');
-    } else if (!form.password) {
-      alert('비밀번호를 입력해주세요');
-    } else if (form.gender === '미선택') {
-      alert('성별을 선택해주세요');
-    } else if (!form.nickname) {
-      alert('닉네임을 입력해주세요');
-    } else if (form.preferredExercise.length === 0) {
-      alert('선호 운동을 입력해주세요');
-    } else {
-      console.log('제출 완료');
-    }
-  };
-
-  const guDong =
-    form.gu && form.dong ? (
-      <span className="gu-dong">
-        {form.gu} {form.dong}
-      </span>
-    ) : (
-      <span className="gu-dong-loading">동네 정보 조회 중</span>
-    );
-
-  const newPreferredExercise = (
-    <div>
-      <select
-        className="exercise"
-        defaultValue="종목"
-        onChange={(e) => {
-          setPreferredExercise({
-            ...preferredExercise,
-            exerciseName: e.target.value,
-          });
-        }}
-      >
-        <option value="종목" hidden>
-          종목
-        </option>
-        <option value="축구">축구</option>
-        <option value="농구">농구</option>
-        <option value="배드민턴">배드민턴</option>
-        <option value="테니스">테니스</option>
-        <option value="탁구">탁구</option>
-        <option value="러닝">러닝</option>
-        <option value="라이딩">라이딩</option>
-      </select>
-      <select
-        className="skill-level"
-        defaultValue="실력"
-        onChange={(e) => {
-          setPreferredExercise({
-            ...preferredExercise,
-            skillLevel: e.target.value,
-          });
-        }}
-      >
-        <option value="실력" hidden>
-          실력
-        </option>
-        <option value="상">상</option>
-        <option value="중">중</option>
-        <option value="하">하</option>
-      </select>
-    </div>
-  );
+  const onClickSignUp = () => {};
+  const onClickKakaoSignUp = () => {};
+  const onClickGoogleSignUp = () => {};
 
   return (
-    <div className="signup">
-      <img className="logo" src={logo} alt="woondongjang logo" />
-      <form>
-        {guDong}
-        <input
-          className="username-input"
-          placeholder="아이디"
-          value={form.username}
-          onChange={(e) => setForm({ ...form, username: e.target.value })}
-        />
-        <input
-          type="password"
-          className="password-input"
-          placeholder="비밀번호"
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-        />
-        <input
-          className="nickname-input"
-          placeholder="닉네임"
-          value={form.nickname}
-          onChange={(e) => setForm({ ...form, nickname: e.target.value })}
-        />
-        <textarea
-          className="introduction-input"
-          placeholder="소개글"
-          value={form.introduction}
-          onChange={(e) => setForm({ ...form, introduction: e.target.value })}
-        />
-        <div className="radio-container">
-          <input
-            id="gender-male"
-            type="radio"
-            name="gender"
-            value="남"
-            checked={form.gender === '남성'}
-            onChange={() => setForm({ ...form, gender: '남성' })}
-          />
-          <label htmlFor="gender-male">남</label>
-          <input
-            id="gender-female"
-            type="radio"
-            name="gender"
-            value="여"
-            checked={form.gender === '여성'}
-            onChange={() => setForm({ ...form, gender: '여성' })}
-          />
-          <label htmlFor="gender-female">여</label>
-        </div>
-        <span>선호 운동</span>
-        {/* <button type="link">추가하기</button> */}
-        {newPreferredExercise}
-        <button className="signup-submit-button" onClick={onClickSubmit}>
-          완료
-        </button>
-      </form>
+    <div className="signup-container">
+      <Layout name="회원가입" imageUrl={defaultImage}>
+        <label htmlFor="username">이름</label>
+        <input id="username" placeholder="이름" />
+        <label htmlFor="password">비밀번호</label>
+        <input id="password" placeholder="비밀번호" />
+        <label htmlFor="password-verify">비밀번호 확인</label>
+        <input id="password-verify" placeholder="비밀번호" />
+        <Button className="local-signup-button" onClick={onClickSignUp}>
+          계정 만들기
+        </Button>
+        <Divider text="or" />
+        <Button
+          className="social-signup-button"
+          imageUrl={kakaotalkIcon}
+          onClick={onClickKakaoSignUp}
+        >
+          카카오 계정으로 회원가입
+        </Button>
+        <Button
+          className="social-signup-button"
+          imageUrl={googleIcon}
+          onClick={onClickGoogleSignUp}
+        >
+          구글 계정으로 회원가입
+        </Button>
+        <span className="signup-instruction">
+          운동장의 회원이신가요?{' '}
+          <span
+            className="signup-button"
+            onClick={() => history.push('/signin')}
+          >
+            로그인 하기
+          </span>
+        </span>
+      </Layout>
     </div>
   );
 };
