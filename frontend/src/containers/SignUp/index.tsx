e8c14a969561cc8d5388ddbb661ad4cc81c86b41
Juyoung <godante27@gmail.com>
2021-12-06 03:43:37 +0900

    feat: Add sign up feature

--- frontend/src/containers/SignUp/index.tsx
+++ frontend/src/containers/SignUp/index.tsx
@@ -9,20 +9,35 @@ import Button from '../../components/Button';
 import './index.scss';
 import googleIcon from '../../assets/image/auth/google.svg';
 import kakaotalkIcon from '../../assets/image/auth/kakaotalk.svg';
+import { SignUpDTO } from '../../backend/entity/user';
 
 interface Props {
   history: History;
 }
 
 const SignUp = ({ history }: Props) => {
-  // const [] = useState();
+  const [signUpForm, setSignUpForm] = useState<SignUpDTO>({
+    username: '',
+    password: '',
+  });
+  const [checkPassword, setCheckPassword] = useState<string>();
+
   const user = window.localStorage.getItem('profileInfo') || null;
 
   useEffect(() => {
     if (user) history.push('/main');
   });
 
-  const onClickSignUp = () => {};
+  const onClickSignUp = () => {
+    if (!signUpForm.username) alert('이름을 입력해주세요');
+    else if (!signUpForm.password) alert('비밀번호를 입력해주세요');
+    else if (signUpForm.password !== checkPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+    } else {
+      // axios.post
+    }
+  };
+
   const onClickKakaoSignUp = () => {};
   const onClickGoogleSignUp = () => {};
 
@@ -30,12 +45,28 @@ const SignUp = ({ history }: Props) => {
     <div className="signup-container">
       <Layout name="회원가입" imageUrl={defaultImage}>
         <label htmlFor="username">이름</label>
-        <input id="username" placeholder="이름" />
+        <input
+          id="username"
+          placeholder="이름"
+          onChange={(e) =>
+            setSignUpForm({ ...signUpForm, username: e.target.value })
+          }
+        />
         <label htmlFor="password">비밀번호</label>
-        <input id="password" placeholder="비밀번호" />
+        <input
+          id="password"
+          placeholder="비밀번호"
+          onChange={(e) =>
+            setSignUpForm({ ...signUpForm, password: e.target.value })
+          }
+        />
         <label htmlFor="password-verify">비밀번호 확인</label>
-        <input id="password-verify" placeholder="비밀번호" />
-        <Button className="local-signup-button" onClick={onClickSignUp}>
+        <input
+          id="password-verify"
+          placeholder="비밀번호"
+          onChange={(e) => setCheckPassword(e.target.value)}
+        />
+        <Button id="local-signup-button" onClick={onClickSignUp}>
           계정 만들기
         </Button>
         <Divider text="or" />
