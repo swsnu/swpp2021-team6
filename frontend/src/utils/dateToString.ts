ba34de00941327068ced57b5f5b045917d1651df
kyungwon-lee <kw375310@gmail.com>
2021-12-08 04:45:36 +0900

    fix: travis

--- frontend/src/utils/dateToString.ts
+++ frontend/src/utils/dateToString.ts
@@ -3,6 +3,7 @@ export const changeDateFormat = (str: string) => {
 
   return `${numArray[0]}년 ${numArray[1]}월 ${numArray[2]}일 ${numArray[3]}:${numArray[4]}`;
 };
+
 export const changeDateFormat2 = (str: string) => {
   const numArray = Array.from(str.matchAll(/[\d]+/g), (x) => x[0]);
 
