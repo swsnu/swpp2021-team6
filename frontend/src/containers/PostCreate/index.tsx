5f7294789e4eccbab0afd3053b9b88a2d2ace36c
Juyoung <godante27@gmail.com>
2021-12-08 04:56:06 +0900

    chore: Add PostCreate css

--- frontend/src/containers/PostCreate/index.tsx
+++ frontend/src/containers/PostCreate/index.tsx
@@ -10,10 +10,16 @@ import getGuDong from '../../utils/getGuDong';
 import { AppState } from '../../store/store';
 import { getKakaoMap } from '../../utils/getKakaoMap';
 import { createPost, readUser } from '../../backend/api/api';
+import whiteExercise from '../../assets/image/icon/white-exercise.svg';
+import whiteLevel from '../../assets/image/icon/white-level.svg';
+import './index.scss';
+import Divider from '../../components/Divider';
+import searchIcon from '../../assets/image/icon/search.svg';
+import participateIcon from '../../assets/image/icon/participate.svg';
 
 const initialPostState: CreatePostEntity = {
-  exerciseName: '축구',
-  expectedLevel: '상관 없음',
+  exerciseName: '종목',
+  expectedLevel: '기대 실력',
   title: '',
   description: '',
   meetAt: '',
@@ -83,7 +89,7 @@ const PostCreate: React.FC = () => {
   };
 
   const onClickSetPlace = () => {
-    const { place_name, x, y, road_address_name, phone } = selectedPlace;
+    const { placeName, x, y, roadAddressName, phone } = selectedPlace;
     getGuDong(x, y).then((value) => {
       const { gu, dong } = value;
       setPost({
@@ -92,10 +98,10 @@ const PostCreate: React.FC = () => {
           ...post.place,
           gu,
           dong,
-          name: place_name,
+          name: placeName,
           latitude: y,
           longitude: x,
-          address: road_address_name,
+          address: roadAddressName,
           telephone: phone,
         },
       });
@@ -103,6 +109,8 @@ const PostCreate: React.FC = () => {
 
     alert('장소 정보가 입력되었습니다');
   };
+
+  console.log(post);
 
   const onClickSearch = () => {
     const container = document.getElementById('map');
@@ -117,143 +125,200 @@ const PostCreate: React.FC = () => {
   };
 
   const onclickSubmit = async () => {
-    if (!post.title) {
-      alert('제목을 입력해주세요');
+    if (post.exerciseName === '종목' || post.expectedLevel === '기대 실력') {
+      alert('운동 종목과 기대 실력을 바르게 입력해주세요.');
+    } else if (!post.title) {
+      alert('제목을 입력해주세요.');
     } else if (!post.description) {
-      alert('설명을 입력해주세요');
+      alert('설명을 입력해주세요.');
     } else if (!post.kakaotalkLink) {
-      alert('카카오톡 오픈채팅방 링크를 입력해주세요');
+      alert('카카오톡 오픈채팅방 링크를 입력해주세요.');
     } else if (!/[open.kakao.com]/.test(post.kakaotalkLink)) {
-      alert('올바른 링크를 입력해주세요');
+      alert('올바른 링크를 입력해주세요.');
     } else if (
       !post.place.name ||
       !post.place.latitude ||
       !post.place.longitude
     ) {
-      alert('운동 장소를 설정해주세요');
+      alert('운동 장소를 설정해주세요.');
     } else {
-      // Get Gu Dong
-      const GuDong = getGuDong(post.place.longitude, post.place.latitude);
-      post.place.gu = (await GuDong).gu;
-      post.place.dong = (await GuDong).dong;
+      const guDong = getGuDong(post.place.longitude, post.place.latitude);
+      post.place.gu = (await guDong).gu;
+      post.place.dong = (await guDong).dong;
 
       const newPost = (await createPost({ createPayload: post })).entity;
       history.push(`/post/${newPost.postId}`);
     }
   };
 
-  return (
-    <form>
-      <h1>새 포스트 작성하기</h1>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <label htmlFor="exerciseType">종목 : </label>
-        <select
-          name="exerciseType"
-          id="exerciseType"
-          value={post.exerciseName}
-          onChange={(e) => setPost({ ...post, exerciseName: e.target.value })}
-        >
-          <option value="축구">축구</option>
-          <option value="농구">농구</option>
-          <option value="배드민턴">배드민턴</option>
-          <option value="테니스">테니스</option>
-          <option value="탁구">탁구</option>
-          <option value="러닝">러닝</option>
-          <option value="라이딩">라이딩</option>
-        </select>
-        <label htmlFor="expectedLevel">모집 실력 : </label>
-        <select
-          name="expectedLevel"
-          id="expectedLevel"
-          defaultValue={post.expectedLevel}
-          onChange={(e) => setPost({ ...post, expectedLevel: e.target.value })}
-        >
-          <option value="상">상</option>
-          <option value="중">중</option>
-          <option value="하">하</option>
-          <option value="상관 없음">상관 없음</option>
-        </select>
-      </div>
+  const onKeyPressSearch = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      onClickSearch();
+    }
+  };
 
-      <input
-        placeholder="제목"
-        onChange={(e) => setPost({ ...post, title: e.target.value })}
-      />
-      <div>
-        <div>Date & Time</div>
-        <DatePicker
-          value={date}
-          onChange={(e) => setDate(e)}
-          allowClear={false}
-        />
-        <TimePicker
-          format="h:mm a"
-          minuteStep={10}
-          defaultValue={moment('7:00', 'h:mm a')}
-          allowClear={false}
-          use12Hours
-          onChange={(e) => setTime(e)}
-        />
-        <div>Num of People</div>
-        <label htmlFor="min-capacity">모집 인원 : 최소 </label>
-        <input
-          id="min-capacity"
-          type="number"
-          min="1"
-          max="10"
-          value={post.minCapacity}
-          onChange={(e) => onChangeMinCapacity(e)}
-        />
-        <label htmlFor="max-capacity"> 명 ~ 최대 </label>
-        <input
-          id="max-capacity"
-          type="number"
-          min="1"
-          max="10"
-          value={post.maxCapacity}
-          onChange={(e) => onChangeMaxCapacity(e)}
-        />
-        <span> 명</span>
+  return (
+    <form id="post-create-container">
+      <div id="header">
+        <h1>새로운 모임 만들기</h1>
+        <div id="select-container">
+          <img src={whiteExercise} alt="exercise icon" />
+          <select
+            name="exerciseType"
+            id="exerciseType"
+            value={post.exerciseName}
+            onChange={(e) => setPost({ ...post, exerciseName: e.target.value })}
+          >
+            <option value="종목" hidden>
+              종목
+            </option>
+            <option value="축구">축구</option>
+            <option value="농구">농구</option>
+            <option value="배드민턴">배드민턴</option>
+            <option value="테니스">테니스</option>
+            <option value="탁구">탁구</option>
+            <option value="러닝">러닝</option>
+            <option value="라이딩">라이딩</option>
+          </select>
+          <img src={whiteLevel} alt="level icon" />
+          <select
+            name="expectedLevel"
+            id="expectedLevel"
+            value={post.expectedLevel}
+            onChange={(e) =>
+              setPost({ ...post, expectedLevel: e.target.value })
+            }
+          >
+            <option value="기대 실력" hidden>
+              기대 실력
+            </option>
+            <option value="상">상</option>
+            <option value="중">중</option>
+            <option value="하">하</option>
+            <option value="상관 없음">상관 없음</option>
+          </select>
+        </div>
       </div>
-      <textarea
-        placeholder="설명"
-        onChange={(e) => setPost({ ...post, description: e.target.value })}
-      />
-      <label>카카오톡 오픈채팅 : </label>
-      <input
-        placeholder="초대 링크를 입력해주세요"
-        onChange={(e) => setPost({ ...post, kakaotalkLink: e.target.value })}
-      />
-      <div className="map-container" style={{ display: 'flex' }}>
-        <div id="map" style={{ width: '500px', height: '350px' }} />
-        <div
-          className="map-container-right"
-          style={{ display: 'flex', flexDirection: 'column' }}
-        >
-          <div>
-            <input
-              placeholder="검색어를 입력하세요"
-              onChange={(e) => setKeyword(e.target.value)}
-            />
-            <button type="button" onClick={onClickSearch}>
-              검색
-            </button>
-          </div>
-          {!selectedPlace && <div>검색어를 입력해 장소를 선택해주세요</div>}
-          {selectedPlace && (
-            <div>
-              <p>{selectedPlace.place_name}</p>
-              <p>{selectedPlace.road_address_name}</p>
-              <p>{selectedPlace.phone}</p>
-              <button type="button" onClick={onClickSetPlace}>
-                확인
-              </button>
+      <div id="post-body">
+        <div className="left">
+          <div className="date box">
+            <span>날짜</span>
+            <div className="date-time-input">
+              <DatePicker
+                id="date-picker"
+                value={date}
+                onChange={(e) => setDate(e)}
+                allowClear={false}
+              />
+              <TimePicker
+                format="h:mm a"
+                minuteStep={10}
+                defaultValue={moment('7:00', 'h:mm a')}
+                allowClear={false}
+                use12Hours
+                onChange={(e) => setTime(e)}
+              />
             </div>
+          </div>
+          <div className="capacity box">
+            <span>인원</span>
+            <div className="capacity-content">
+              <div>
+                <label htmlFor="min-capacity">최소</label>
+                <input
+                  id="min-capacity"
+                  type="number"
+                  min="1"
+                  max="10"
+                  value={post.minCapacity}
+                  onChange={(e) => onChangeMinCapacity(e)}
+                />
+              </div>
+              <div>
+                <label htmlFor="max-capacity">최대</label>
+                <input
+                  id="max-capacity"
+                  type="number"
+                  min="1"
+                  max="10"
+                  value={post.maxCapacity}
+                  onChange={(e) => onChangeMaxCapacity(e)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="title shadow box">
+            <span>제목</span>
+            <input
+              id="title-input"
+              placeholder="제목을 입력해주세요"
+              onChange={(e) => setPost({ ...post, title: e.target.value })}
+            />
+          </div>
+          <div className="introduction shadow box">
+            <span>설명</span>
+            <input
+              id="introduction-input"
+              placeholder="운동 모임에 대한 설명을 입력해주세요"
+              onChange={(e) =>
+                setPost({ ...post, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="kakaotalk-link shadow box">
+            <span>카카오톡 오픈채팅</span>
+            <input
+              id="kakaotalk-link-input"
+              placeholder="오픈 채팅방 주소를 입력해주세요"
+              onChange={(e) =>
+                setPost({ ...post, kakaotalkLink: e.target.value })
+              }
+            />
+          </div>
+        </div>
+      </div>
+      <div id="map-container" className="box">
+        <span>위치</span>
+        <div id="map-container-content">
+          <div id="content-top">
+            <div id="map" style={{ width: '500px', height: '350px' }} />
+            <div id="map-container-right">
+              <div id="search-container">
+                <input
+                  placeholder="운동할 장소를 입력해주세요 ex. 서울대학교 테니스장"
+                  onChange={(e) => setKeyword(e.target.value)}
+                  onKeyPress={onKeyPressSearch}
+                />
+                <button type="button" onClick={onClickSearch}>
+                  <img src={searchIcon} alt="search" />
+                </button>
+              </div>
+              <div />
+            </div>
+          </div>
+          {selectedPlace && (
+            <>
+              <Divider />
+              <div id="selected-place">
+                <div>
+                  <span id="place-name">{selectedPlace.placeName}</span>
+                  <span id="place-address">
+                    {selectedPlace.roadAddressName}
+                  </span>
+                </div>
+                <button type="button" onClick={onClickSetPlace}>
+                  선택
+                </button>
+              </div>
+            </>
           )}
         </div>
       </div>
-      <button type="button" onClick={onclickSubmit}>
-        등록하기
+      <button id="submit-button" type="button" onClick={onclickSubmit}>
+        <img src={participateIcon} alt="participate" />
+        모임 만들기
       </button>
     </form>
   );
