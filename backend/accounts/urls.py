0939ee44039276f91e8d06600bf39a42154e0ae1
kyungwon-lee <55077010+kyungwon-lee@users.noreply.github.com>
2021-12-10 18:15:43 +0900

    hotfix: sonar cloud fix (#138)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("signup", views.signup, name="signup"),
     path("signin", views.signin, name="signin"),
     path("signout", views.signout, name="signout"),
+    path("get/<int:user_id>", views.get_user_detail, name = "get_user_detail"),
     path("<int:user_id>", views.user_detail, name="user_detail"),
     path("<int:user_id>/notification", views.get_notification, name="get_notification"),
     path(
