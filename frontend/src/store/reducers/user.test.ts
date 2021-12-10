af6c1cd860fad964ecbd714e1fb55940e7c9be92
Juyoung <godante27@gmail.com>
2021-12-11 03:03:46 +0900

    test: Implement user reducer test with coverage 100%

--- frontend/src/store/reducers/user.test.ts
+++ frontend/src/store/reducers/user.test.ts
@@ -1,7 +1,93 @@
-import userReducer from './user';
+import reducer from './user';
+import * as actionTypes from '../actions/actionTypes';
+import {
+  NotificationEntity,
+  NotiType,
+} from '../../backend/entity/notification';
+
+const stubNotification: NotificationEntity[] = [
+  {
+    notiId: 1,
+    notiType: NotiType.comment,
+    postId: 1,
+    postTitle: 'test post',
+    isRead: false,
+    createdAt: '1분 전',
+  },
+];
 
 describe('userReducer', () => {
-  it('should', () => {
-    //
+  it('should return default state', () => {
+    const newState = reducer(undefined, {});
+    expect(newState).toEqual({
+      loginUserId: null,
+      notification: null,
+    });
+  });
+
+  it('should auto signin user', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.AUTO_SIGNIN,
+      userId: 1,
+    });
+    expect(newState).toEqual({
+      loginUserId: 1,
+      notification: null,
+    });
+  });
+
+  it('should signin user', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.SIGNIN,
+      userId: 1,
+    });
+    expect(newState).toEqual({
+      loginUserId: 1,
+      notification: null,
+    });
+  });
+
+  it('should signup user', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.ONBOARDING,
+      userId: 1,
+    });
+    expect(newState).toEqual({
+      loginUserId: 1,
+      notification: null,
+    });
+  });
+
+  it('should signout user', () => {
+    const newState = reducer(
+      { loginUserId: 1, notification: null },
+      { type: actionTypes.SIGNOUT },
+    );
+    expect(newState).toEqual({
+      loginUserId: null,
+      notification: null,
+    });
+  });
+
+  it('should get notification', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.GET_NOTIFICATION,
+      notification: stubNotification,
+    });
+    expect(newState).toEqual({
+      loginUserId: null,
+      notification: stubNotification,
+    });
+  });
+
+  it('should read notification', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.READ_NOTIFICATION,
+      notification: stubNotification,
+    });
+    expect(newState).toEqual({
+      loginUserId: null,
+      notification: stubNotification,
+    });
   });
 });
