382080035c0bab7406ead75b856aa6d85e5a952e
silvermanseoul <silverman.unicorn@gmail.com>
2021-12-12 13:11:04 +0900

    chore: Install django-cors-headers

--- backend/woondongjang/settings.py
+++ backend/woondongjang/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -152,6 +154,8 @@ LOGGING = {
     },
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 secrets_file = os.path.join(BASE_DIR, "secrets.json")
 try:
