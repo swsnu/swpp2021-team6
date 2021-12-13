29a3bbf6f0bed9ca4951b0e105564a0fa9c6a3b0
Juyoung <godante27@gmail.com>
2021-12-14 00:45:51 +0900

    test: Add PostEdit container test

--- frontend/src/containers/PostEdit/index.test.tsx
+++ frontend/src/containers/PostEdit/index.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import * as reactRedux from 'react-redux';
+import * as API from '../../backend/api/api';
 import PostEdit from '.';
 import mockStore from '../../store/store';
 
@@ -43,19 +44,26 @@ jest.mock('react-router', () => ({
   useHistory: () => ({ push: mockPush }),
   useParams: () => ({ id: '1' }),
 }));
+
+const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+window.alert = jest.fn();
+const confirmMock = jest.spyOn(window, 'confirm');
+
 describe('PostCreate', () => {
   let postEdit: any;
-
+  let readPostMock: any;
+  let updatePostMock: any;
   beforeEach(() => {
     postEdit = (
       <Provider store={mockStore}>
         <PostEdit />
       </Provider>
     );
-    const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
-    useSelectorMock.mockImplementation((callback) =>
-      callback({ user: { loginUserId: 1 } }),
-    );
+
+    readPostMock = jest
+      .spyOn(API, 'readPost')
+      .mockResolvedValue({ entity: mockPost });
+    updatePostMock = jest.spyOn(API, 'updatePost').mockResolvedValue();
   });
 
   afterEach(() => {
@@ -67,7 +75,9 @@ describe('PostCreate', () => {
   });
 
   it('should render without error', () => {
-    const component = mount(postEdit);
+    useSelectorMock.mockImplementation((callback) =>
+      callback({ user: { loginUserId: 1 } }),
+    );
 
     useStateMock
       .mockReturnValueOnce([mockPost, setPostMock])
@@ -75,6 +85,90 @@ describe('PostCreate', () => {
         { title: '종목', description: '실력' },
         setPostUpdateMock,
       ]);
+    const component = mount(postEdit);
     expect(component.find('#post-edit-container').length).toBe(1);
+  });
+
+  it('should alert when login user is not the host', () => {
+    useSelectorMock.mockImplementation((callback) =>
+      callback({ user: { loginUserId: 2 } }),
+    );
+
+    useStateMock
+      .mockReturnValueOnce([mockPost, setPostMock])
+      .mockReturnValueOnce([
+        { title: '종목', description: '실력' },
+        setPostUpdateMock,
+      ]);
+    const component = mount(postEdit);
+  });
+
+  it('should handle input change', () => {
+    confirmMock.mockReturnValue(true);
+
+    useSelectorMock.mockImplementation((callback) =>
+      callback({ user: { loginUserId: 1 } }),
+    );
+    useStateMock
+      .mockReturnValueOnce([mockPost, setPostMock])
+      .mockReturnValueOnce([{ title: '', description: '' }, setPostUpdateMock]);
+    const component = mount(postEdit);
+
+    component
+      .find('#title-input')
+      .simulate('change', { target: { value: 'title change' } });
+    component
+      .find('#introduction-input')
+      .simulate('change', { target: { value: 'description change' } });
+
+    component.find('#edit-submit-button').simulate('click');
+  });
+
+  it('should alert on clicking cancel button', () => {
+    confirmMock.mockReturnValue(true);
+    useSelectorMock.mockImplementation((callback) =>
+      callback({ user: { loginUserId: 1 } }),
+    );
+    useStateMock
+      .mockReturnValueOnce([mockPost, setPostMock])
+      .mockReturnValueOnce([{ title: '', description: '' }, setPostUpdateMock]);
+    const component = mount(postEdit);
+
+    component
+      .find('#title-input')
+      .simulate('change', { target: { value: 'title change' } });
+    component
+      .find('#introduction-input')
+      .simulate('change', { target: { value: 'description change' } });
+
+    component.find('#edit-cancel-button').simulate('click');
+  });
+
+  it('should verify form on submit', () => {
+    useSelectorMock.mockImplementation((callback) =>
+      callback({ user: { loginUserId: 1 } }),
+    );
+    useStateMock
+      .mockReturnValueOnce([mockPost, setPostMock])
+      .mockReturnValueOnce([
+        { title: 'title', description: 'description' },
+        setPostUpdateMock,
+      ]);
+    const component = mount(postEdit);
+
+    component
+      .find('#title-input')
+      .simulate('change', { target: { value: '' } });
+
+    component.find('#edit-submit-button').simulate('click');
+
+    component
+      .find('#title-input')
+      .simulate('change', { target: { value: 'title' } });
+    component
+      .find('#introduction-input')
+      .simulate('change', { target: { value: '' } });
+
+    component.find('#edit-submit-button').simulate('click');
   });
 });
