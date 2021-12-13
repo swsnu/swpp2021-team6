e94f4cec341b79275293246d255a232d9543871f
Juyoung Bae <godante27@gmail.com>
2021-12-14 04:26:18 +0900

    Frontend/test/notification improve (#163)
    * hotfix: fix post detail
    
    * test: Fix Notification container test
    
    * Revert "Merge remote-tracking branch 'origin/frontend/test/notification' into frontend/test/notification"
    
    This reverts commit bc67d1d462b70ddfe0a86f900f6a510a4ddde46a.
    
    Co-authored-by: SunnyBaik <qortmdfuf007@hotmail.com>

--- frontend/src/containers/Notification/index.test.tsx
+++ frontend/src/containers/Notification/index.test.tsx
@@ -1,73 +1,39 @@
 /* eslint-disable object-shorthand */
-import React, { useEffect } from 'react';
 import { mount } from 'enzyme';
 import * as reactRedux from 'react-redux';
 import { Provider } from 'react-redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { mockNavigatorGeolocation } from '../../test-utils/mockNavigatorGeolocation';
-import userInfo from '../../mocks/userInfo.json';
-import notification from '../../mocks/notification.json';
-import Profile from '.';
+import mockNotis from '../../mocks/notification.json';
+import Notification from '.';
 import { history } from '../../store/store';
 
-window.alert = jest.fn().mockImplementation();
-const useStateMock = jest.spyOn(React, 'useState');
-const setUserInfoMock = jest.fn();
-const setUpdateProfileStateMock = jest.fn();
-const setSelectedExerciseMock = jest.fn();
-const setGuDongMock = jest.fn();
 const mockPush = jest.fn();
-
 const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
   useHistory: () => ({ push: mockPush }),
 }));
 
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useHistory: () => ({
-//     push: mockPush,
-//   }),
-// }));
-
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useEffect: jest.fn(),
-}));
-
-const spyAlert = jest.spyOn(window, 'alert').mockImplementation();
-const spyHistoryPush = jest
-  .spyOn(history, 'push')
-  .mockImplementation(jest.fn());
-
 const mockStore = createStore(
   combineReducers({
     router: connectRouter(history),
-    user: (state = { user: null, notification: notification }, action) => state,
+    user: (state = { loginUsrId: 1, notification: mockNotis }, action) => state,
   }),
   applyMiddleware(routerMiddleware(history)),
 );
 
 describe('Notification', () => {
-  const mockUserInfo = userInfo;
-
   let noti: any;
-  let spyHistoryPush: any;
-  // let getGuDong: any;
+
   beforeEach(() => {
     noti = (
       <Provider store={mockStore}>
-        <Profile />
+        <Notification />
       </Provider>
     );
-
-    useSelectorMock.mockImplementation((callback) =>
-      callback({ user: { user: userInfo } }),
-    );
-    spyHistoryPush = jest.spyOn(history, 'push').mockImplementation(jest.fn());
   });
 
   afterEach(() => {
@@ -78,40 +44,24 @@ describe('Notification', () => {
   });
 
   it('should render without error', () => {
-    useStateMock.mockReturnValueOnce([mockUserInfo, setUserInfoMock]);
+    useDispatchMock.mockImplementation(() => jest.fn());
+    useSelectorMock.mockReturnValue({
+      loginUserId: 1,
+      notification: mockNotis,
+    });
     const component = mount(noti);
-    expect(component.find('.profile').length).toBe(0);
+    expect(component.find('.dropdown').length).toBeTruthy();
   });
 
-  // it('should push to profile edit', () => {
-  //   useStateMock.mockReturnValueOnce([mockUserInfo, setUserInfoMock]);
-  //   const component = mount(noti);
-  //   const wrapper = component.find('.button-container');
-  //   wrapper.find('.edit-profile-button').simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(1);
-  // });
-
-  // it('should push to signout', () => {
-  //   useStateMock.mockReturnValueOnce([mockUserInfo, setUserInfoMock]);
-  //   const component = mount(profile);
-  //   const wrapper = component.find('.button-container');
-  //   wrapper.find('.signout-button').simulate('click');
-  //   expect(spyHistoryPush).toBeCalledWith('/signout');
-  // });
-
-  // it('should push to a post', () => {
-  //   useStateMock.mockReturnValueOnce([mockUserInfo, setUserInfoMock]);
-  //   const component = mount(noti);
-  //   const wrapper = component.find('.noti-item-container');
-  //   wrapper.at(0).simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(1);
-  //   wrapper.at(1).simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(2);
-  //   wrapper.at(2).simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(3);
-  //   wrapper.at(3).simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(4);
-  //   wrapper.at(4).simulate('click');
-  //   expect(spyHistoryPush).toBeCalledTimes(5);
-  // });
+  it('should dispatch getNotification', () => {
+    useDispatchMock.mockImplementation(() => jest.fn());
+    useSelectorMock.mockReturnValueOnce({
+      loginUserId: 1,
+      notification: mockNotis,
+    });
+    const component = mount(noti);
+    component.find('.ant-dropdown-trigger').simulate('click');
+    component.find('.noti-item-container').at(0).simulate('click');
+    expect(mockPush).toBeCalledTimes(1);
+  });
 });
